f699971db47e18143298497feb93461267bca2c9
hanseop <hanseop95@gmail.com>
2020-03-16 15:20:02 +0900

    [기능 추가] collectionView cell random color 적용
    램덤컬러를 반환하는 generateRandomColor() 메소드 작성했습니다.
    알파값이 0.5부터 주어진 이유는 너무 낮으면 색상 표현이 안돼서 0.5 이상으로 설정했습니다.

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -11,11 +11,34 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
-    let dataManager = 
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
-        collectionView.dataSource =
+        collectionView.dataSource = self
     }
+    
+    private func generateRandomColor() -> UIColor {
+        let range: Range<CGFloat> = (1..<255)
+        let red = CGFloat.random(in: range) / 255
+        let green = CGFloat.random(in: range) / 255
+        let blue = CGFloat.random(in: range) / 255
+        let alpha = CGFloat.random(in: 0.5..<1)
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
+        let color = generateRandomColor()
+        print(color)
+        cell.backgroundColor = color
+        return cell
+    }
+}
