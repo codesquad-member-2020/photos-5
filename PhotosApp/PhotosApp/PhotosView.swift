98ae87cf8345b93c93e29b0de1d77c0045a8bb55
hanseop <hanseop95@gmail.com>
2020-03-16 17:46:37 +0900

    [기능 추가] view 클래스 생성
    viewController의 최상위 view객체를 생성

---
+++ PhotosApp/PhotosApp/PhotosView.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosView.swift
+//  PhotosApp
+//
+//  Created by 신한섭 on 2020/03/16.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class PhotosView: UIView {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+}
