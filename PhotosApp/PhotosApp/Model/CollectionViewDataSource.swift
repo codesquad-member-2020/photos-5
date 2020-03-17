35cd29407c00b789e6a88978c44b1e01cee0ae85
GangWoon <gangwoon2018@gmail.com>
2020-03-17 21:16:11 +0900

    [기능 추가] CollectionViewCell에 사진 표시
    Photos를 처리하는 객체를 만들건지 고민했었습니다.
    객체를 만들어서 처리한다면 cell를 photos객체로 넘겨야했고, collectionViewCell을 처리하기 위해서 UIKit을 내포해야됩니다.
    더 확신이 든 이유는 requestImage(for: , targetSize: , contentMode: , options: , resultHandler: )를 사용해서 이미지를 설정해야 되는데 cell을 받아오던가, closure로 넘겨서 처리를 하는 부분이 너무 부자연스러웠습니다.

--- PhotosApp/PhotosApp/Model/CollectionViewDataSource.swift
+++ PhotosApp/PhotosApp/Model/CollectionViewDataSource.swift
@@ -7,27 +7,43 @@
 //
 
 import UIKit
+import Photos
 
 class CollectionViewDataSource: NSObject {
     
-    private let count: Int
+    private let imageManager: PHCachingImageManager!
+    private var fetchResult: PHFetchResult<PHAsset>!
+    private let photoOptions: PHFetchOptions!
     
-    public init(count: Int) {
-        self.count = count
+    public override init() {
+        imageManager = PHCachingImageManager()
+        photoOptions = PHFetchOptions()
+        photoOptions.sortDescriptors = [
+            NSSortDescriptor(key: "creationDate", ascending: true)
+        ]
+        fetchResult = PHAsset.fetchAssets(with: photoOptions)
     }
 }
 
+
 extension CollectionViewDataSource: UICollectionViewDataSource {
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return count
+        return fetchResult.count
     }
     
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(
             withReuseIdentifier: UICollectionViewCell.photosCellIdentifier,
             for: indexPath
-            ) as? PhotoCell else {return UICollectionViewCell()}
-        cell.backgroundColor = .randomColor
+            ) as? PhotoCell else { return UICollectionViewCell() }
+        imageManager.requestImage(
+            for: fetchResult[indexPath.item],
+            targetSize: cell.frame.size,
+            contentMode: .aspectFill,
+            options: .none
+        ) { image, _ in
+            cell.apply(with: image)
+        }
         return cell
     }
 }
