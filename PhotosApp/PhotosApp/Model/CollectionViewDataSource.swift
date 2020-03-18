9438d61f4700bd0ff5538480641f49daca0adb8c
GangWoon <gangwoon2018@gmail.com>
2020-03-18 18:49:02 +0900

    [기능 수정] PhotoManager 객체 생성
    역할을 분리했기 때문에 그에 적합하게 코드를 변경했습니다.

--- PhotosApp/PhotosApp/Model/CollectionViewDataSource.swift
+++ PhotosApp/PhotosApp/Model/CollectionViewDataSource.swift
@@ -7,43 +7,23 @@
 //
 
 import UIKit
-import Photos
 
 class CollectionViewDataSource: NSObject {
     
-    private let imageManager: PHCachingImageManager
-    private var fetchResult: PHFetchResult<PHAsset>
-    private let photoOptions: PHFetchOptions
-    
-    public override init() {
-        imageManager = PHCachingImageManager()
-        photoOptions = PHFetchOptions()
-        photoOptions.sortDescriptors = [
-            NSSortDescriptor(key: "creationDate", ascending: true)
-        ]
-        fetchResult = PHAsset.fetchAssets(with: photoOptions)
-    }
+    let photoManager = PhotoManager()
 }
-
 
 extension CollectionViewDataSource: UICollectionViewDataSource {
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return fetchResult.count
+        return photoManager.count
     }
     
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: UICollectionViewCell.photosCellIdentifier,
+            withReuseIdentifier: PhotoCell.identifier,
             for: indexPath
             ) as? PhotoCell else { return UICollectionViewCell() }
-        imageManager.requestImage(
-            for: fetchResult[indexPath.item],
-            targetSize: cell.frame.size,
-            contentMode: .aspectFill,
-            options: .none
-        ) { image, _ in
-            cell.apply(with: image)
-        }
+        photoManager.load(index: indexPath.item, size: cell.frame.size, cell: cell)
         return cell
     }
 }
