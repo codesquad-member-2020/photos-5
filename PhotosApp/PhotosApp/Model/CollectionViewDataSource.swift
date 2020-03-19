87c2275d4ffb81da0c3d4413e7136879a52aead8
hanseop <hanseop95@gmail.com>
2020-03-19 18:45:32 +0900

    [기능 수정]  이미지 삽입 방법 개선
    현재 인덱스패스와 넣고자하는 이미지의 인덱스값과 같은지를 비교하고, 같으면 이미지를 넣어주도록 수정

--- PhotosApp/PhotosApp/Model/CollectionViewDataSource.swift
+++ PhotosApp/PhotosApp/Model/CollectionViewDataSource.swift
@@ -27,9 +27,10 @@ extension CollectionViewDataSource: UICollectionViewDataSource {
             withReuseIdentifier: PhotoCell.identifier,
             for: indexPath
             ) as? PhotoCell else { return UICollectionViewCell() }
-        photoManager.load(index: indexPath.item, size: cell.frame.size, cell: cell)
+        let collectionViewIndexPathItem: Int = collectionView.indexPathForItem(at: CGPoint(x: cell.frame.origin.x, y: cell.frame.origin.y))?.item ?? 0
+        if collectionViewIndexPathItem == indexPath.item {
+            photoManager.load(index: indexPath.item, size: cell.frame.size, cell: cell)
+        }
         return cell
     }
 }
-
-
