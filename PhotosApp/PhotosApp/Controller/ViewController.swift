da5d67a3d05cd77a43be3fde718ed619c69e0db0
hanseop <hanseop95@gmail.com>
2020-03-20 13:29:24 +0900

    [기능 추가] collectionView item변경 처리
    asset이 변경되었다는 notification을 관찰함
    해당 변경사항이 removed인지 inserted인지 changed인지를 파악하여 collectionView 업데이트
    UI를 업데이트하기 때문에 main 스레드에서 처리해줌

--- PhotosApp/PhotosApp/Controller/ViewController.swift
+++ PhotosApp/PhotosApp/Controller/ViewController.swift
@@ -13,11 +13,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     private let datasource = CollectionViewDataSource()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = datasource
         authorizationStatus()
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(reloadAsset(_:)),
+                                               name: Notification.Name.needToAssetReload,
+                                               object: nil)
     }
     
     func authorizationStatus() {
@@ -43,6 +46,28 @@ class ViewController: UIViewController {
             
         default:
             fatalError("권한 없음")
+        }
+    }
+    
+    @objc func reloadAsset(_ notification: Notification) {
+        if let userInfo = notification.userInfo {
+            DispatchQueue.main.async {
+                self.collectionView.performBatchUpdates({
+                    if userInfo["removed"] != nil {
+                        guard let removed = userInfo["removed"] as? IndexSet else {return}
+                        self.collectionView.deleteItems(at: removed.map { IndexPath(item: $0, section:0) })
+                    }
+                    
+                    if userInfo["inserted"] != nil{
+                        guard let inserted = userInfo["inserted"] as? IndexSet else {return}
+                        self.collectionView.insertItems(at: inserted.map { IndexPath(item: $0, section:0) })
+                    }
+                    if userInfo["changed"] != nil {
+                        guard let changed = userInfo["changed"] as? IndexSet else {return}
+                        self.collectionView.reloadItems(at: changed.map { IndexPath(item: $0, section:0) })
+                    }
+                })
+            }
         }
     }
 }
